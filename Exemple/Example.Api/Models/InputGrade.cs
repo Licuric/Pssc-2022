b45dede5d653e984cd23e6929698b40eb8586999
gdeeniss <denis.gurita@yahoo.com>
2023-01-09 16:53:55 +0200

    APi kinda works

---
+++ Exemple/Example.Api/Models/InputGrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Exemple.Domain.Models;
+
+namespace Example.Api.Models
+{
+    public class InputGrade
+    {
+        public string RegistrationNumber { get; set; }
+
+        public decimal Exam { get; set; }
+
+        public decimal Activity { get; set; }
+    }
+}
